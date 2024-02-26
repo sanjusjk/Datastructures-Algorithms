5d135d5f3d6f568b8158c551f82099589ccbdcde
Sanjeev Jyothikrishnan E <sanju.jyothikrishnan@gmail.com>
2024-02-26 18:16:41 +0530

    Add project files.

---
+++ Queues/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ *      Queues are FIFO Data structure. that means the elements inserted first are retrieved first unlike a Stack.
+ * 
+ *      Queues are Linear Data Structure. 1 by 1
+ *     
+ *      queues has methods TryPeek and TryDequeue.
+ *      
+ *      Queues offer methods like Enqueue() and Dequeue() to insert and retrieve respectively
+ * 
+ *      Queues can also be used with methods of Collections as it implements Icollection interface
+ * 
+ *       
+ *      Uses:
+ *      Queues are used in LinkedList, PriorityQueues, Breadth-First Search Algorithms
+ *      Keyboard Buffer (order of letter pressed)
+ *      Printer Queues
+ */
+
+//Examples:
+
+
+Queue<string> queue = new Queue<string>();
+
+queue.Enqueue("a");
+queue.Enqueue("b");
+queue.Enqueue("c");
+queue.Enqueue("d");
+
+Console.WriteLine(queue.ElementAt(2)+"\n");
+
+
+while (queue.Count > 0)
+{
+    Console.WriteLine(queue.Dequeue());
+}
+
+var ss = queue.TryPeek(out string res);
+var sss = queue.TryDequeue(out string res1);
+
+
+Console.WriteLine(ss + "\n");
+Console.WriteLine(sss + "\n");
---
+++ stack/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Declare stack of ty
+Stack<string> stack = new Stack<string>();
+
+//inserts entry in stack
+stack.Push("Sanju");
+stack.Push("Sanju1");
+stack.Push("Sanju2");
+stack.Push("Sanju3");
+stack.Push("Sanju4");
+
+//removes the last element from the stack and assign to the variable something
+var something = stack.Pop();
+
+// prints the variable something
+Console.WriteLine(something);
+
+
+// lets us view the last element without poping from stack.
+Console.WriteLine(stack.Peek());
+
+
